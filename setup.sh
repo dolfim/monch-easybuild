25016fbcb7b12c62ba226a3960438e8d00070ebd
Michele Dolfi <dolfim@phys.ethz.ch>
2016-10-14 13:42:12 +0200

    updates in setup.sh

--- setup.sh
+++ setup.sh
@@ -13,18 +13,20 @@ fi
 #export EASYBUILD_REPOSITORYPATH="git@git.comp.phys.ethz.ch:codes/monch-easybuild.git,ebfiles_repo/"$hostName
 
 GROUP_EBPATH=/apps/monch/p501/RH6.7
+SCRATCH_EBPATH=$SCRATCH/apps/
 HOME_EBPATH=$HOME/apps/eb_$hostName
 
 if [ -z "$1" ]; then
     echo "You have to pass the PREFIX as argument. You can select one of the standard paths below or quit."
     PS3="Select a PREFIX path: "
-    options=("$HOME_EBPATH" "$GROUP_EBPATH" "Quit")
+    options=("$SCRATCH_EBPATH" "$HOME_EBPATH" "$GROUP_EBPATH" "Quit")
     select opt in "${options[@]}"; do 
         case "$REPLY" in
     
-        1 ) PROJ=$HOME_EBPATH; break;;
-        2 ) PROJ=$GROUP_EBPATH; break;;
-        3 ) return ;;
+        1 ) PROJ=$SCRATCH_EBPATH; break;;
+        2 ) PROJ=$HOME_EBPATH; break;;
+        3 ) PROJ=$GROUP_EBPATH; break;;
+        4 ) return ;;
         q ) return ;;
     
         *) echo "Invalid option. Try another one.";continue;;
@@ -45,5 +47,8 @@ export EASYBUILD_ROBOT_PATHS="$REPO_ROOT/easyconfigs/:$EASYBUILD_ROBOT_PATHS:"
 export EASYBUILD_INCLUDE_EASYBLOCKS=$REPO_ROOT'/easyblocks/*py,'$EASYBUILD_INCLUDE_EASYBLOCKS
 
 ## Use the group paths
-module use /apps/monch/UES/RH6.7-16.04/easybuild/modules/all
+#module use /apps/monch/UES/RH6.7-16.04/easybuild/modules/all
 module use $GROUP_EBPATH/easybuild/modules/all
+# for slurm module:
+module use /apps/monch/UES/modulefiles/
+
